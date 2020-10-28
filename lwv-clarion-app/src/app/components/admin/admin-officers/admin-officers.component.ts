21819152af0a79a32c04b55c244f4c52b8c8a95d
chrissycotton16 <c.n.cotton@eagle.clarion.edu>
2020-10-28 15:22:33 -0400

    Officer CRUD completed & small cleanup with member

--- lwv-clarion-app/src/app/components/admin/admin-officers/admin-officers.component.ts
+++ lwv-clarion-app/src/app/components/admin/admin-officers/admin-officers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { OfficerService } from 'src/app/services/officer.service';
 import { MatDialog } from '@angular/material/dialog';
 import { Officer } from 'src/app/models/officer';
+import { OfficerAddDialogComponent } from './officer-add-dialog/officer-add-dialog.component';
+import { OfficerUpdateDialogComponent } from './officer-update-dialog/officer-update-dialog.component';
 
 @Component({
   selector: 'app-admin-officers',
@@ -9,17 +11,50 @@ import { Officer } from 'src/app/models/officer';
   styleUrls: ['./admin-officers.component.scss']
 })
 export class AdminOfficersComponent implements OnInit {
-  displayedColumns: string[] = ['FirstName', 'LastName', 'Position', 'Email', 'TermStart'];
-  @Input() officer ={ OfficerID:0, FirstName:'', LastName:'', Position:'', Email:'', TermStart:'', TermEnd:''}; //this is for storing purposes
+  displayedColumns: string[] = ['FirstName', 'LastName', 'Position', 'Email', 'TermStart', 'Edit'];
+  @Input() officer ={ OfficerID:0, FirstName:'', LastName:'', Position:'', Email:'', TermStart:''}; //this is for storing purposes
   officers: Officer[];
   officerLength: number;
   error = '';
   success = '';
+  @Input() newOfficer: Officer ={ OfficerID:0, FirstName:'', LastName:'', Position:'', Email:'', TermStart:''};
+  officerToUpdate: Officer ={ OfficerID:0, FirstName:'', LastName:'', Position:'', Email:'', TermStart:''};
+
   constructor(private officerService: OfficerService, public dialog:MatDialog){
     this.ngOnInit();
   }
 
   ngOnInit(){
+    this.getOfficers();
+  }
+
+  openDialog(): void{
+    const dialogRef = this.dialog.open(OfficerAddDialogComponent, {
+      width: '450px',
+      data:{ newOfficer: this.newOfficer },
+      autoFocus: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.newOfficer = result;
+      if(result != undefined){
+        this.addOfficer(this.newOfficer);
+      }
+    });
+  } 
+
+  addOfficer(off:Officer){
+    off = this.newOfficer;
+    this.resetErrors();
+    this.officerService.store(off)
+      .subscribe(
+        (res: Officer[]) => {
+          // Update the list of officers
+          this.officers = res;
+          // Inform the user
+          this.success = 'Created successfully';
+        },
+        (err) => this.error = err
+      );
     this.getOfficers();
   }
 
@@ -35,4 +70,53 @@ export class AdminOfficersComponent implements OnInit {
     );
   }
 
+  deleteOfficer(OfficerID) {
+    if(window.confirm('Are you sure you want to delete this item?')){
+      this.resetErrors();
+      this.officerService.delete(+OfficerID)
+        .subscribe(
+          (res: Officer[]) => {
+            this.officers = res;
+            this.success = 'Deleted successfully';
+          },
+          (err) => this.error = err
+        );
+      this.getOfficers();
+    }
+  }
+
+  openUpdateDialog(off){
+    const dialogRef = this.dialog.open(OfficerUpdateDialogComponent, {
+      width: '450px',
+      data:{ updateOfficerID: off.OfficerID, updateFirstName: off.FirstName, updateLastName: off.LastName, updatePosition: off.Position,  
+             updateEmail: off.Email, updateTermStart: off.TermStart },
+      autoFocus: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result != undefined){
+        this.officerToUpdate = result;
+        this.updateOfficer(this.officerToUpdate);
+      }
+    });
+    this.getOfficers();
+  }
+
+  updateOfficer(off){
+    this.resetErrors();
+    this.officerService.update({OfficerID: off.OfficerID, FirstName: off.FirstName, LastName: off.LastName, 
+        Position: off.Position, Email: off.Email, TermStart: off.TermStart })
+    .subscribe(
+      (res) => {
+        this.officers    = res;
+        this.success = 'Updated successfully';
+      },
+      (err) => this.error = err
+    );
+  }
+
+
+  resetErrors() {
+    this.success = '';
+    this.error   = '';
+  }
 }
--- lwv-clarion-app/src/app/services/officer.service.ts
+++ lwv-clarion-app/src/app/services/officer.service.ts
@@ -20,6 +20,49 @@ export class OfficerService {
         catchError(this.handleError));
     }
 
+    delete(OfficerID: number): Observable<Officer[]> {
+        const params = new HttpParams()
+        .set('OfficerID', OfficerID.toString());
+
+        return this.http.delete(`${this.baseUrl}/delete`, { params: params })
+        .pipe(map(res => {
+            const filteredMembers = this.officers.filter((officer) => {
+            return +officer['OfficerID'] !== +OfficerID;
+            });
+            return this.officers = filteredMembers;
+        }),
+        catchError(this.handleError));
+    }
+
+    store(officer: Officer): Observable<Officer[]> {
+        console.log("in officer service");
+        return this.http.post(`${this.baseUrl}/store`, { data: officer })
+        .pipe(map((res) => {
+            this.officers.push(res['data']);
+            return this.officers;
+        }),
+        catchError(this.handleError));
+    }
+
+    update(officer: Officer): Observable<Officer[]> {
+        console.log(officer);
+        return this.http.put(`${this.baseUrl}/update`, { data: officer })
+        .pipe(map((res) => {
+            const theOfficer = this.officers.find((item) => {
+            return +item['OfficerID'] === +officer['OfficerID'];
+            });
+            if (theOfficer) {
+                theOfficer['FirstName'] = officer['FirstName'];
+                theOfficer['LastName'] = officer['LastName'];
+                theOfficer['Position'] = officer['Position'];
+                theOfficer['Email'] = officer['Email'];
+                theOfficer['TermStart'] = officer['TermStart'];
+            }
+            return this.officers;
+        }),
+        catchError(this.handleError));
+    } 
+
     private handleError(error: HttpErrorResponse) {
         console.log(error);
 
