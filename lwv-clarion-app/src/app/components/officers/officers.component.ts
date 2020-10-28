7c1875c1ea6bd99b3ebf076d53d74edd97c8f6d2
chrissycotton16 <c.n.cotton@eagle.clarion.edu>
2020-10-28 13:36:34 -0400

    quick fix- added officers get() to tab and admin

--- lwv-clarion-app/src/app/components/officers/officers.component.ts
+++ lwv-clarion-app/src/app/components/officers/officers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Officer } from 'src/app/models/officer';
+import { OfficerService } from 'src/app/services/officer.service';
 
 @Component({
   selector: 'app-officers',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./officers.component.scss']
 })
 export class OfficersComponent implements OnInit {
+  officers: Officer [];
+  displayedColumns: string[] = ['FirstName', 'LastName', 'Position', 'Email', 'TermStart'];
 
-  constructor() { }
+  constructor(private officerService: OfficerService) { }
 
   ngOnInit(): void {
+    this.getOfficers();
   }
 
+  getOfficers():void {
+    this.officerService.getAll().subscribe(
+      (res: Officer[]) => {
+        this.officers = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
