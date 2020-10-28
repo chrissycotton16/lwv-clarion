ea18502b85d20e81c80ff5f05b1fade8cf233b73
kalyncombetty <kcombetty@gmail.com>
2020-10-28 13:09:36 -0400

    added admin login dialog

--- lwv-clarion-app/src/app/components/log-in/log-in.component.ts
+++ lwv-clarion-app/src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData{
+  
+}
+
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +13,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<LogInComponent>, 
+    private dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
   }
 
+  logIn(){
+    this.dialogRef.close();
+  }
+
+  close(){
+    this.dialogRef.close();
+  }
 }
