99cc57ea72d913fe29a161e23789d52292d80df5
chrissycotton16 <c.n.cotton@eagle.clarion.edu>
2020-11-04 19:04:09 -0500

    Revert "admin log in works!"
    This reverts commit 78144352c53e5b19beed46d3f4154fd2a6b4d6d7.

--- lwv-clarion-app/src/app/components/admin/admin.component.ts
+++ lwv-clarion-app/src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Admin } from 'src/app/models/admin';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +10,29 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  ngOnInit(): void {}
+  user = '';
+  password = '';
+  @Input() adminToCheck: Admin ={ AdminID:0, user:'', password:''};
+  constructor(public adminService: AdminService, private router: Router){
+    this.ngOnInit();
+  }
+
+  ngOnInit() {}
+  
+  logIn(user, password){
+    console.log(this.user + " " + this.password);
+    console.log(user, password);
+    this.adminToCheck = {user: this.user, password: this.password};
+
+    console.log(this.adminToCheck);
+    this.adminService.userLogin(this.adminToCheck).pipe(first()).subscribe(
+      data =>
+      {
+        alert("user name or password is correct");
+      },
+      error => 
+      {
+        alert("user name or password is incorrect");
+      });
+  }
 }
